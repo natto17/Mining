9f023f6ad44d0969aea96f4cafa8de9c3dbd2a13
Tuney123 <nam17@hampshire.edu>
2019-09-16 00:36:33 -0400

    part one is done!

---
+++ Mining_NatMong/Assets/scripts/controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    int loopOnce = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { if(loopOnce == 0 && Time.time > 3.0)
+        {
+            print("It's been three seconds!");
+            loopOnce += 1;
+        }
+        
+    }
+}
