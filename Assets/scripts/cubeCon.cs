475c13aaafc5a689fc5de404da559c2058a08512
natto17 <48194106+natto17@users.noreply.github.com>
2019-10-18 11:15:53 -0400

    new upload of files
    deleted wrong repository, had to move all of the files from one folder to another. game works okay with new spot

---
+++ Assets/scripts/cubeCon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeCon : MonoBehaviour
+{
+    public Ore myOre;
+
+    // Start is called before the first frame update
+    void Start()
+    { }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+
+        if (myOre== Ore.Bronze)
+        {
+            controller.bronze--;
+            controller.points += controller.bronzePoints;
+            print("You scored 1 point!");
+        }
+        else if(myOre == Ore.Silver)
+        {
+            controller.silver--;
+            controller.points += controller.silverPoints;
+            print("You scored 10 points!");
+        }
+        else if (myOre == Ore.Gold)
+        {
+            controller.gold--;
+            controller.points += controller.goldPoints;
+            print("You scored 100 points!");
+        }
+        }
+    }
